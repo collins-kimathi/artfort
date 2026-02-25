0cf750a7aa80d5e7853650b8b13e0ae7a9bf5832
Collins Kimathi <collinskimathi49@gmail.com>
2026-02-25 16:39:59 +0300

    homechanged

--- src/components/HomeStatements.jsx
+++ src/components/HomeStatements.jsx
@@ -1,6 +1,10 @@
 // HomeStatements component/page file.
 const lines = [
   'Welcome to our shop, where every paint you see is lovingly handcrafted by a passionate and talented artist dedicated to quality and color',
+  <br />,
+  'Our paints are not just colors, but a reflection of our commitment to excellence and creativity,designed to inspire and bring your artistic',
+  <br />,
+  'visions to life',
 ]
 
 function HomeStatements() {
